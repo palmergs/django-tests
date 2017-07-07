a9781a24f8f705eef452a438d0a3cb66bd54eacd
Galen Palmer <galen.palmer@contrastsecurity.com>
2017-07-07 12:22:16 -0400

    validation in prereq checks for cycles

--- tutorial/goals/models.py
+++ tutorial/goals/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 
@@ -17,3 +18,29 @@ class Prereq(models.Model):
     postreq = models.ForeignKey(Goal, null=False, on_delete=models.CASCADE, related_name='prerequisites')
     created_at = models.DateTimeField(null=False, default=timezone.now)
 
+
+    def save(self, **kwargs):
+        self.clean()
+        return super(Prereq, self).save(**kwargs)
+
+
+    def clean(self):
+        left_arr = [ self.prereq ]
+        right_arr = [ self.postreq ]
+        for goal in left_arr:
+            for p in goal.prerequisites.all():
+                if p.prereq in right_arr:
+                    raise ValidationError('Prereq found in post-requisite list')
+                left_arr.append(p.prereq)
+                print(len(left_arr))
+        for goal in right_arr:
+            for p in goal.dependents.all():
+                if p.postreq in left_arr:
+                    raise ValidationError('Post-requisite found in prereq list')
+                right_arr.append(p.postreq)
+                print(len(right_arr))
+        
+
+
+
+        
