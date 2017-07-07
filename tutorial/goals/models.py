a1d6d88352ae299ea92093459e063457088241ca
Galen Palmer <galen.palmer@contrastsecurity.com>
2017-07-07 11:42:34 -0400

    initial models with migrations allow for relations

--- tutorial/goals/models.py
+++ tutorial/goals/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Goal(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=False, blank=True, default='')
+    created_at = models.DateTimeField(null=False, default=timezone.now)
+    completed_at = models.DateTimeField(null=True)
+
+    def completed(self):
+        return self.completed_at != None
+
+class Prereq(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=True, default='')
+    description = models.TextField(null=False, blank=True, default='')
+    prereq = models.ForeignKey(Goal, null=False, on_delete=models.CASCADE, related_name='dependents')
+    postreq = models.ForeignKey(Goal, null=False, on_delete=models.CASCADE, related_name='prerequisites')
+    created_at = models.DateTimeField(null=False, default=timezone.now)
+
