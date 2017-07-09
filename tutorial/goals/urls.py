dde726f76094cbb64644e86f0c00703d2fd36d62
Galen Palmer <palmergs@gmail.com>
2017-07-08 23:13:28 -0400

    initial templates for index; wip for update page

--- tutorial/goals/urls.py
+++ tutorial/goals/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'goals'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'^(?P<goal_id>[0-9]+)$', views.show, name='show'),
+	url(r'^(?P<goal_id>[0-9]+)/complete$', views.complete, name='complete'),
 ]
 
--- tutorial/goals/views.py
+++ tutorial/goals/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.utils import timezone
+from django.shortcuts import render
+
+from .models import Goal
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the goals index.")
+	goals = Goal.objects.all()
+	template = loader.get_template('goals/index.html')
+	context = { 'goals': goals }
+	return render(request, 'goals/index.html', context)
 
 
+def show(request, goal_id):
+	return HttpResponse("You're looking at goal %s." % goal_id)
+
+
+def complete(request, goal_id):
+	goal = Goal.objects.get(id=goal_id)
+	msg = 'unchanged'
+	if goal.completed():
+		goal.completed_at = None
+		goal.save()
+		msg = 'not completed'
+	elif goal.prerequisites_completed():
+		goal.completed_at = timezone.now()
+		goal.save()
+		msg = 'completed'
+	
+	return HttpResponse("The goal is %s." % msg)
+
+
+			
